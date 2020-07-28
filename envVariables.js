3fc0d0159e658cc0750e60f86df4f14a84eb9c9e
Akash Shah <aks301190@gmail.com>
2020-07-28 23:04:21 +0530

    Add logic to handle requiredIf.

--- envVariables.js
+++ envVariables.js
@@ -154,50 +154,87 @@ let enviromentVariables = {
   },
 }
 
-let success = true;
+
+const validRequiredIfOperators = [
+  "EQUALS",
+  "NOT_EQUALS"
+];
+
+let environmentVariablesCheckSuccessful = true;
 
 module.exports = function() {
   Object.keys(enviromentVariables).forEach(eachEnvironmentVariable=>{
   
     let tableObj = {
-      [eachEnvironmentVariable] : ""
+      [eachEnvironmentVariable] : "PASSED"
     };
   
-    if( !(process.env[eachEnvironmentVariable]) && !(enviromentVariables[eachEnvironmentVariable].optional)) {
-      success = false;
+    let keyCheckPass = true;
 
-      if(enviromentVariables[eachEnvironmentVariable] && enviromentVariables[eachEnvironmentVariable].message !== "") {
+
+    if(enviromentVariables[eachEnvironmentVariable].optional === true
+      && enviromentVariables[eachEnvironmentVariable].requiredIf
+      && enviromentVariables[eachEnvironmentVariable].requiredIf.key
+      && enviromentVariables[eachEnvironmentVariable].requiredIf.key != ""
+      && enviromentVariables[eachEnvironmentVariable].requiredIf.operator
+      && validRequiredIfOperators.includes(enviromentVariables[eachEnvironmentVariable].requiredIf.operator)
+      && enviromentVariables[eachEnvironmentVariable].requiredIf.value
+      && enviromentVariables[eachEnvironmentVariable].requiredIf.value != "") {
+        switch (enviromentVariables[eachEnvironmentVariable].requiredIf.operator) {
+          case "EQUALS":
+            if(process.env[enviromentVariables[eachEnvironmentVariable].requiredIf.key] === enviromentVariables[eachEnvironmentVariable].requiredIf.value) {
+              enviromentVariables[eachEnvironmentVariable].optional = false;
+            }
+            break;
+          case "NOT_EQUALS":
+              if(process.env[enviromentVariables[eachEnvironmentVariable].requiredIf.key] != enviromentVariables[eachEnvironmentVariable].requiredIf.value) {
+                enviromentVariables[eachEnvironmentVariable].optional = false;
+              }
+              break;
+          default:
+            break;
+        }
+    }
+      
+    if(enviromentVariables[eachEnvironmentVariable].optional === false) {
+      if(!(process.env[eachEnvironmentVariable])
+        || process.env[eachEnvironmentVariable] == "") {
+        environmentVariablesCheckSuccessful = false;
+        keyCheckPass = false;
+      } else if (enviromentVariables[eachEnvironmentVariable].possibleValues
+        && Array.isArray(enviromentVariables[eachEnvironmentVariable].possibleValues)
+        && enviromentVariables[eachEnvironmentVariable].possibleValues.length > 0) {
+        if(!enviromentVariables[eachEnvironmentVariable].possibleValues.includes(process.env[eachEnvironmentVariable])) {
+          environmentVariablesCheckSuccessful = false;
+          keyCheckPass = false;
+          enviromentVariables[eachEnvironmentVariable].message += ` Valid values - ${enviromentVariables[eachEnvironmentVariable].possibleValues.join(", ")}`
+        }
+      }
+    }
+
+    if((!(process.env[eachEnvironmentVariable])
+      || process.env[eachEnvironmentVariable] == "")
+      && enviromentVariables[eachEnvironmentVariable].default
+      && enviromentVariables[eachEnvironmentVariable].default != "") {
+      process.env[eachEnvironmentVariable] = enviromentVariables[eachEnvironmentVariable].default;
+    }
+
+    if(!keyCheckPass) {
+      if(enviromentVariables[eachEnvironmentVariable].message !== "") {
         tableObj[eachEnvironmentVariable] = 
         enviromentVariables[eachEnvironmentVariable].message;
       } else {
-        tableObj[eachEnvironmentVariable] = "required";
+        tableObj[eachEnvironmentVariable] = `FAILED - ${eachEnvironmentVariable} is required`;
       }
-    } else {
-
-      tableObj[eachEnvironmentVariable] = "success";
-
-      if( 
-        enviromentVariables[eachEnvironmentVariable].possibleValues &&
-        !enviromentVariables[eachEnvironmentVariable].possibleValues.includes(process.env[eachEnvironmentVariable])
-      ) {
-        tableObj[eachEnvironmentVariable] += ` Valid values - ${enviromentVariables[eachEnvironmentVariable].possibleValues.join(", ")}`;
-      } else {
-        
-        if(enviromentVariables[eachEnvironmentVariable].optional) {
-          tableObj[eachEnvironmentVariable] = enviromentVariables[eachEnvironmentVariable].message;
-        }
-
-      }
-      
     }
 
     tableData.push(tableObj);
   })
 
-  LOGGER.info(tableData.toString());
+  console.log(tableData.toString());
 
   return {
-    success : success
+    success : environmentVariablesCheckSuccessful
   }
 }
 

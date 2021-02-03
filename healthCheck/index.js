661067496db8273f4edfdfc8a4cf679fbc63386e
aman-tunerlabs <aman@tunerlabs.com>
2021-02-03 10:26:18 +0530

    Added health check apis

---
+++ healthCheck/index.js
@@ -0,0 +1,13 @@
+/**
+ * name : index.js.
+ * author : Aman Karki.
+ * created-date : 02-Feb-2021.
+ * Description : Health check Root file.
+*/
+
+// Dependencies
+let healthCheckService = require("./healthCheckService");
+
+module.exports = function (app) {
+    app.get("/healthCheckStatus",healthCheckService.healthCheckStatus);
+}
